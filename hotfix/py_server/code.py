b24d1850a71e591419b3e9e1a5bd4c3c434bf237
zhourui@digisky.com <zhourui@digisky.com>
2017-11-24 17:29:25 +0800

    在Http头添加了文件的大小

--- hotfix/py_server/code.py
+++ hotfix/py_server/code.py
@@ -2,7 +2,8 @@ import web
 import os
 BUF_SIZE = 262144
 urls = (
-    '/packageUrl/hello.html', 'packageUrl',
+    '/packageUrl/1.png', 'packageUrl',
+	'/packageUrl/2.zip', 'packageUrl2',
 	'/remoteManifestUrl', 'remoteManifestUrl',
 	'/remoteVersionUrl', 'remoteVersionUrl'
 )
@@ -17,12 +18,37 @@ if __name__ == "__main__":
 
 class packageUrl:
 	def GET(self):
-		file_name = 'hello.html'
+		file_name = '1.png'
 		file_path = os.path.join('file', file_name)
 		print file_name
 		f = None
 		try:
 			f = open(file_path, "rb")
+			web.header('Content-Length',370000)
+			web.header('Content-Type','application/octet-stream')
+			web.header('Content-disposition', 'attachment; filename=%s' % file_name)
+			while True:
+				c = f.read(BUF_SIZE)
+				if c:
+					yield c
+				else:
+					break
+		except Exception, e:
+			print e
+			yield 'Error'
+		finally:
+			if f:
+				f.close()
+				
+class packageUrl2:
+	def GET(self):
+		file_name = '2.zip'
+		file_path = os.path.join('file', file_name)
+		print file_name
+		f = None
+		try:
+			f = open(file_path, "rb")
+			web.header('Content-Length',370000000)
 			web.header('Content-Type','application/octet-stream')
 			web.header('Content-disposition', 'attachment; filename=%s' % file_name)
 			while True:
